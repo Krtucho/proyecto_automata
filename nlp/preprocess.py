19d5eddd7ba337d32d30d99e86c5894cdf9bac88
Krtucho <Krtucho98@gmail.com>
2023-01-06 06:18:40 -0500

    example in test.py, sentence_segmentation, tokenizing, POSTag

--- nlp/preprocess.py
+++ nlp/preprocess.py
@@ -1,4 +1,7 @@
-import nltk
+from abc import abstractmethod
+import nltk, spacy
+nlp = spacy.load('es_core_news_md')
+
 
 class PreprocessText():
     def __init__(self, text, sent_seg="nltk", word_tok="nltk") -> None:
@@ -29,3 +32,27 @@ class PreprocessText():
         # unigram_tagger = nltk.UnigramTagger(sentences, backoff=default_tagger)
         # bigram_tagger = nltk.BigramTagger(sentences, backoff=unigram_tagger)
         # trigram_tagger = nltk.TrigramTagger(sentences, backoff=bigram_tagger)
+
+    @staticmethod
+    def sentence_segmentation(document: str, language="spanish")->list:
+        return nltk.sent_tokenize(document, language="spanish")
+
+    @staticmethod
+    def tokenize_and_tag_sent(sent:str):
+        """Dada una oracion (sent:str) Devuelve una lista de tuplas de la forma (text, lemma, pos) con los tokens que formen parte de esta"""
+        document = nlp(sent)
+
+    # for token in document:
+    #     print(token.text, token.lemma_, token.pos_, token.dep_)
+
+        return [(token.text, token.lemma_, token.pos_) for token in document]
+
+    @staticmethod
+    def get_verbs_and_nouns(sent:str):
+        """Dada una oracion (sent:str) Devuelve una lista de tuplas de la forma (text, lemma, pos) con los tokens que sean sustantivos o verbos"""
+        targets = ["NOUN", "VERB"]
+        return [(t,l,p) for (t,l,p) in PreprocessText.tokenize_and_tag_sent(sent) if p in targets]
+    
+    # @staticmethod
+    # def process_text(text: str):
+    #     return [(t,l,p) for (t,l,p) in (PreprocessText.get_verbs_and_nouns(sent) for sent in PreprocessText.sentence_segmentation(text))]
