9bb072e175d405c5d5703979c85cc050965a11fc
Krtucho <Krtucho98@gmail.com>
2023-01-06 06:39:19 -0500

    example of use in main.py, merging feature/nlp and feature/expert-system

--- system/pyke3-1.1.1/pyke-1.1.1/main.py
+++ system/pyke3-1.1.1/pyke-1.1.1/main.py
@@ -3,10 +3,19 @@ sys.path.append('/home/krtucho/School/IA+Sim/github/proyecto_automata/pyke3-1.1.
 
 import examples.system.driver as driver
 
+from preprocess import PreprocessText
+sentences = PreprocessText.sentence_segmentation("¿Cuáles son los síntomas que presenta el cáncer de páncreas?")
+
+chunks_list = []
+for sentence in sentences:
+    chunks_list.append(PreprocessText.get_verbs_and_nouns(sentence))
+
 # Cuales son los sintomas que presenta el cancer de pancreas
-chunks = ["sintoma", "presenta", "cancer", "pancreas"]
+# chunks = ["sintoma", "presenta", "cancer", "pancreas"]
 
 # Para los casos de palabras compuestas, tendremos que tomar el chunk como una, ej
 # Dolor de algo......Se hace necesario tomar un chunk como ambas palabras unidas
 # uses bc_example.krb
-driver.bc_test('dolor_en_abdomen', chunks) 
+
+for chunks in chunks_list:
+    driver.bc_test('dolor_en_abdomen', chunks) 
