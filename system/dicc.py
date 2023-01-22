ff78f311dfa30e8744f1e61cc5f7d5db193bedfe
Krtucho <Krtucho98@gmail.com>
2023-01-22 13:45:24 -0500

    symptoms-frecuencies

---
+++ system/dicc.py
@@ -0,0 +1,101 @@
+
+from pyke_utils.main import *
+
+dicc_partes_cuerpos_sintomas={}
+dicc_partes_cuerpos_sintomas["en la cabeza"]=[]
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("dolores_de_cabeza")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("problemas_de_vision")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("perdida_gradual_de_la_sensibilidad")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("dificultades_con_el_habla")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("cansancio")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("confusion_en_asuntos_diarios")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("dificultad_para_tomar_decisiones")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("incapacidad_para_seguir_ordenes_sencillas")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("cambios_en_la_personalidad_o_el_comportamiento")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("convulsiones")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("problemas_de_audicion")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("fiebre")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("fatiga")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("falta_de_aire")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("dolor_de_oidos")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("dificultad_para_respirar_por_la_nariz")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("perdida_del_sentido_del_olfato")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("sangrados_nasales")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("secrecion_de_la_nariz")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("dolor_o_hinchazon_facial")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("dificultad_para_abrir_la_boca")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("dolor_irradiado_hacia_el_oido")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("ojos_llorosos")
+dicc_partes_cuerpos_sintomas["en la cabeza"].append("coloracion_amarillenta_de_la_piel_y_los_ojos")
+
+
+dicc_partes_cuerpos_sintomas["en el estomago"]=[]
+dicc_partes_cuerpos_sintomas["en el estomago"].append("perdida_de_peso")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("dolor_o_hinchazon_o_presion_en_abdomen")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("intolerancia_a_la_glucosa")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("esteatorrea")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("dolor_o_hinchazon_o_presion_o_bulto_en_pelvis")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("dificultad_para_comer")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("sensacion_de_llenura")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("problemas_gastrointestinales")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("gases")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("distension_abdominal")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("estrennimiento")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("nauseas_o_vomitos")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("perdida_del_apetito")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("un_agrandamiento_del_higado")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("picazon")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("hinchazon_o_acumulacion_de_liquido_en_abdomen")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("fatiga")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("anemia")
+dicc_partes_cuerpos_sintomas["en el estomago"].append("acidez_estomacal_o_indigestion")
+
+dicc_partes_cuerpos_sintomas["en las extremidades"]=[]
+dicc_partes_cuerpos_sintomas["en las extremidades"].append("perdida_de_peso")
+dicc_partes_cuerpos_sintomas["en las extremidades"].append("hinchazon_de_piernas")
+dicc_partes_cuerpos_sintomas["en las extremidades"].append("hinchazon_de_los_tobillos_y_las_piernas")
+dicc_partes_cuerpos_sintomas["en las extremidades"].append("perdida_gradual_de_la_sensibilidad")
+
+
+dicc_partes_cuerpos_sintomas["en el pecho"]=[]
+dicc_partes_cuerpos_sintomas["en el pecho"].append("presion_arterial_alta")
+dicc_partes_cuerpos_sintomas["en el pecho"].append("dificultad_para_respirar")
+dicc_partes_cuerpos_sintomas["en el pecho"].append("sensibilidad_o_aumento_de_tamanno_de_las_mamas")
+dicc_partes_cuerpos_sintomas["en el pecho"].append("aparicion_de_nodulo")
+dicc_partes_cuerpos_sintomas["en el pecho"].append("dolor_o_presion_o_ardor_en_el_pecho")
+dicc_partes_cuerpos_sintomas["en el pecho"].append("retraccion_de_la_piel_del_pezon")
+dicc_partes_cuerpos_sintomas["en el pecho"].append("secrecion_sanguinolenta_del_pezon")
+
+
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"]=[]
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("dolor_o_hinchazon_o_presion_o_bulto_en_pelvis")
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("necesidad_de_orinar_frecuentemente")
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("sangre_en_la_orina")
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("bulto_indoloro_o_hinchazon_en_testiculos")
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("dolor_molestias_o_entumecimiento_en_testiculos")
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("sensacion_de_pesadez_en_el_escroto")
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("dolor_sordo_en_la_parte_del_abdomen_o_la_ingle")
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("acumulacion_subita_de_liquido_en_el_escroto")
+dicc_partes_cuerpos_sintomas["en la parte baja de la cintura"].append("dolor_region_lumbar_inferior")
+
+dicc_partes_cuerpos_sintomas["en la espalda"]=[]
+dicc_partes_cuerpos_sintomas["en la espalda"].append("masa_o_bulto_en_el_costado_o_la_espalda")
+dicc_partes_cuerpos_sintomas["en la espalda"].append("dolor_o_presion_en_el_costado_o_la_espalda")
+dicc_partes_cuerpos_sintomas["en la espalda"].append("picazon")
+dicc_partes_cuerpos_sintomas["en la espalda"].append("dolor_en_espalda")
+
+
+dicc_partes_cuerpos_sintomas["en el cuello"]=[]
+dicc_partes_cuerpos_sintomas["en el cuello"].append("dolor_garganta_que_no_desaparece")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("tos_constante")
+
+dicc_partes_cuerpos_sintomas["en el cuello"].append("disfagia")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("protuberancia_o_masa_en_el_cuello")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("llaga_o_lesion_en_el_paladar")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("bulto_en_el_cuello")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("tos_o_ronquera")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("disfonia")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("dolor_a_la_deglucion")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("cambios_en_la_voz")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("hinchazon_de_los_ganglios_linfaticos_en_el_cuello")
+dicc_partes_cuerpos_sintomas["en el cuello"].append("dolor_en_el_cuello_y_en_la_garganta")
